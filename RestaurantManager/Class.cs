1eb79182e63c7108764bbd468d2bc40123e92427
Hoang Viet Ngoc <ngoch2329@gmail.com>
2025-10-08 02:37:43 +0700

    Initial commit

---
+++ RestaurantManager/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace RestaurantManager
+{
+    public class Class
+    {
+    }
+}
---
+++ RestaurantManager/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantManager.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Username { get; set; }
+
+        [Required, StringLength(100)]
+        public string Password { get; set; }
+
+        [StringLength(50)]
+        public string First_Name { get; set; }
+
+        [StringLength(50)]
+        public string Last_Name { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        public DateTime? Last_Login_At { get; set; }
+
+        public bool Is_Actived { get; set; }
+
+        public int Role_Id { get; set; }
+    }
+}
